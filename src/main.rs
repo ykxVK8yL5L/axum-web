bb5ee99473c62e3793a2f2d4584c804798db21ec
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2021-12-20 13:32:42 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,86 @@
-fn main() {
-    println!("Hello, world!");
+//! Run with
+//!
+//! ```not_rust
+//! cargo run -p example-templates
+//! ```
+
+use askama::Template;
+use axum::{
+    body::{self, Full},
+    extract,
+    http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    routing::{get_service,get},
+    Router,
+};
+use std::net::SocketAddr;
+use tower_http::{services::ServeDir, trace::TraceLayer};
+
+#[tokio::main]
+async fn main() {
+    // Set the RUST_LOG, if it hasn't been explicitly defined
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "example_templates=debug")
+    }
+    tracing_subscriber::fmt::init();
+
+    // build our application with some routes
+    let app = Router::new()
+        .nest(
+            "/assets",
+            get_service(ServeDir::new("public/assets")).handle_error(|error: std::io::Error| async move {
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Unhandled internal error: {}", error),
+                )
+            }),
+        )
+        .route("/greet/:name", get(greet))
+        .route("/", get(root));
+
+    // run it
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn greet(extract::Path(name): extract::Path<String>) -> impl IntoResponse {
+    let template = HelloTemplate { name };
+    HtmlTemplate(template)
+}
+
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct HelloTemplate {
+    name: String,
+}
+
+struct HtmlTemplate<T>(T);
+
+impl<T> IntoResponse for HtmlTemplate<T>
+where
+    T: Template,
+{
+    fn into_response(self) -> Response {
+        match self.0.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(body::boxed(Full::from(format!(
+                    "Failed to render template. Error: {}",
+                    err
+                ))))
+                .unwrap(),
+        }
+    }
 }
