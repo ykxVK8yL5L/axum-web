18120aa5dd1f131d0829df4c10d80ef89fd6935a
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2021-12-24 11:16:04 +0800

    Add files via upload

---
+++ src/utils/template.rs
@@ -0,0 +1,45 @@
+use askama::Template;
+use axum::{
+    body::{self, Full},
+    http::{StatusCode},
+    response::{Html, IntoResponse, Response},
+};
+
+
+
+#[derive(Template)]
+#[template(path = "index.html")]
+pub struct HelloTemplate {
+    pub name: String,
+}
+
+
+#[derive(Template)]
+#[template(path = "error.html")]
+pub struct ErrorTemplate {
+    pub label: String,
+    pub message:String,
+}
+
+
+
+
+
+pub struct HtmlTemplate<T>(pub T);
+impl<T> IntoResponse for HtmlTemplate<T>
+where
+    T: Template,
+{
+    fn into_response(self) -> Response {
+        match self.0.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(body::boxed(Full::from(format!(
+                    "Failed to render template. Error: {}",
+                    err
+                ))))
+                .unwrap(),
+        }
+    }
+}
