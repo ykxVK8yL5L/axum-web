1a0beeee911de7805766e5156cfd2e085b294bd1
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2021-12-24 11:16:35 +0800

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::net::{ToSocketAddrs};
+use std::{env};
+use structopt::StructOpt;
+use tracing::{info,Level};
+use tracing_subscriber::FmtSubscriber;
+
+mod config;
+mod utils;
+mod controllers;
+mod routes;
+
+#[tokio::main]
+async fn main() {
+    if env::var("RUST_LOG").is_err() {
+      env::set_var("RUST_LOG", "axum-web=info");
+    }
+    let subscriber = FmtSubscriber::builder().with_max_level(Level::INFO).finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    let app = routes::create_router();
+    // run it
+    let config = config::env::ServerConfig::from_args();
+    let addr = (config.host, config.port)
+              .to_socket_addrs()
+              .unwrap()
+              .next()
+              .unwrap();
+    info!("请在浏览器上打开http://:{:?}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
