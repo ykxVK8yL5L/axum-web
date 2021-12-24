539a679a35aceaa589ec1dcb1ac2db0d8ffdec25
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2021-12-24 19:30:27 +0800

    Update mod.rs

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -1,6 +1,23 @@
+use axum::{
+  extract::Extension,
+  http::StatusCode,
+};
+use crate::{
+  db::Pool,
+  models,
+  constants,
+};
 pub mod index;
 
-pub async fn home() -> &'static str {
-    "Hello, World!"
+
+
+pub async fn home(Extension(pool): Extension<Pool>,) -> Result<String, (StatusCode, String)> {
+  match models::user::User::find_user_by_username("hello",&pool.get().unwrap()) {
+    Ok(user) => {
+      let response = serde_json::to_string(&user).unwrap();
+      Ok(response)
+    } ,  
+    Err(err) => Ok("not found user by name hello".to_string()),
   }
+}
   
