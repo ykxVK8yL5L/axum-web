92226869673062841a8b35e5745102b62edc4a69
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2021-12-24 11:18:33 +0800

    Add files via upload

---
+++ src/controllers/index.rs
@@ -0,0 +1,17 @@
+use crate::utils::template::{HtmlTemplate,HelloTemplate};
+use axum::{
+    extract,
+    response::{IntoResponse},
+};
+
+
+pub async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+
+
+pub async fn greet(extract::Path(name): extract::Path<String>) -> impl IntoResponse {
+  let hellotemplate =HelloTemplate { name };
+  HtmlTemplate(hellotemplate)
+}
