93c42203a45d7bfd02c44cd72f7569e6b1dbb2b5
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2021-12-24 19:31:20 +0800

    Add files via upload

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,1 +1,4 @@
-
+pub mod response;
+pub mod user;
+pub mod user_token;
+pub mod login_history;
---
+++ src/models/user_token.rs
@@ -0,0 +1,35 @@
+use crate::models::user::LoginInfoDTO;
+use chrono::Utc;
+use jsonwebtoken::{
+    EncodingKey,
+    Header
+};
+use serde::{ Deserialize, Serialize };
+
+pub static KEY: [u8; 16] = *include_bytes!("../secret.key");
+static ONE_WEEK: i64 = 60 * 60 * 24 * 7; // in seconds
+
+#[derive(Serialize, Deserialize)]
+pub struct UserToken {
+    // issued at
+    pub iat: i64,
+    // expiration
+    pub exp: i64,
+    // data
+    pub user: String,
+    pub login_session: String,
+}
+
+impl UserToken {
+    pub fn generate_token(login: &LoginInfoDTO) -> String {
+        let now = Utc::now().timestamp_nanos() / 1_000_000_000; // nanosecond -> second
+        let payload = UserToken {
+            iat: now,
+            exp: now + ONE_WEEK,
+            user: login.username.clone(),
+            login_session: login.login_session.clone(),
+        };
+
+        jsonwebtoken::encode(&Header::default(), &payload, &EncodingKey::from_secret(&KEY)).unwrap()
+    }
+}
