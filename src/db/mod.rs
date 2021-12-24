b90cab7b0a2846777cfddf0cd3a2307b53f22258
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2021-12-24 20:16:58 +0800

    Update mod.rs

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -4,11 +4,11 @@ use diesel::r2d2::{self, ConnectionManager};
 pub type Connection = SqliteConnection;
 pub type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
 
-pub fn init_db() -> Pool {
+pub fn init_db(dbname:String) -> Pool {
     dotenv::dotenv().ok();
     // set up database connection pool
-    let connspec = std::env::var("DATABASE_URL").expect("DATABASE_URL");
-    let manager = ConnectionManager::<SqliteConnection>::new(connspec);
+    //let connspec = std::env::var("DATABASE_URL").expect("DATABASE_URL");
+    let manager = ConnectionManager::<SqliteConnection>::new(dbname);
     let pool = r2d2::Pool::builder()
         .build(manager)
         .expect("Failed to create pool.");
